01f5d404be8046d78fe9df2cd41506d5278eeb12
WarinCode <warinsaipanya471@outlook.co.th>
2025-11-26 21:42:43 +0700

    First commit

---
+++ week1/src/K4.kt
@@ -0,0 +1,19 @@
+fun main(){
+    val firstNumber = 10
+    val secondNumber = 5
+    val thirdNumber = 8
+
+    val result = add(firstNumber, secondNumber)
+    val anotherResult = subtract(firstNumber, thirdNumber)
+
+    println("$firstNumber + $secondNumber = $result")
+    println("$firstNumber - $thirdNumber = $anotherResult")
+}
+
+fun add(x: Int, y: Int): Int {
+    return x + y
+}
+
+fun subtract(x: Int, y: Int): Int {
+    return x - y
+}
