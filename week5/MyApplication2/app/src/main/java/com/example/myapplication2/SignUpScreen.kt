5b9ef80f8ba124e24fde5d9cf32c0ea7f9e3875b
WarinCode <warinsaipanya471@outlook.co.th>
2026-01-06 16:29:04 +0700

    Update MainActivity.kt

--- week5/MyApplication2/app/src/main/java/com/example/myapplication2/SignUpScreen.kt
+++ week5/MyApplication2/app/src/main/java/com/example/myapplication2/SignUpScreen.kt
@@ -4,6 +4,7 @@ package com.example.myapplication2
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
